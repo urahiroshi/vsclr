affde9f27d35295f27578fea40765e0d96bc7a51
urahiroshi <asj.hiroshi@gmail.com>
2018-11-04 21:54:38 +0900

    Fix to keep original json

--- index.js
+++ index.js
@@ -5,33 +5,45 @@ const crypto = require('crypto');
 const vsCodeDir = '.vscode';
 const configFile = 'settings.json';
 
-const rand = () => (crypto.randomBytes(1).readUInt8(0));
-const rand3 = [0, 0, 0].map(rand);
-const selected = rand() % 3;
-const selectedVal = rand3[selected];
-const nonSelecteds = [0, 1, 2].filter(i => i !== selected);
-const nonSelectedVals = nonSelecteds.map(i => (
-  Math.floor(
-    rand3[i] *
-    ((256 - selectedVal) / (rand3[nonSelecteds[0]] + rand3[nonSelecteds[1]]))
-  )
-));
-const rgb = [0, 1, 2].map(i => (
-  (i === selected) ? selectedVal : nonSelectedVals[nonSelecteds.indexOf(i)]
-)).map(val => (
-  `0${val.toString(16)}`.slice(-2)
-)).join('');
-
-const config = {
-  'workbench.colorCustomizations': {
-    'statusBar.background': `#${rgb}`,
-  }
+const randomRGB = () => {
+  const rand = () => (crypto.randomBytes(1).readUInt8(0));
+  const rand3 = [0, 0, 0].map(rand);
+  const selected = rand() % 3;
+  const selectedVal = rand3[selected];
+  const nonSelecteds = [0, 1, 2].filter(i => i !== selected);
+  const nonSelectedVals = nonSelecteds.map(i => (
+    Math.floor(
+      rand3[i] *
+      ((256 - selectedVal) / (rand3[nonSelecteds[0]] + rand3[nonSelecteds[1]]))
+    )
+  ));
+  const rgb = [0, 1, 2].map(i => (
+    (i === selected) ? selectedVal : nonSelectedVals[nonSelecteds.indexOf(i)]
+  )).map(val => (
+    `0${val.toString(16)}`.slice(-2)
+  )).join('');
+  return `#${rgb}`;
 };
 
 if (!fs.existsSync(vsCodeDir)) {
   fs.mkdirSync(vsCodeDir);
 }
-fs.writeFileSync(
-  path.join(vsCodeDir, configFile),
-  JSON.stringify(config, undefined, 4)
-);
+
+let config = {};
+const configPath = path.join(vsCodeDir, configFile);
+if (fs.existsSync(configPath)) {
+  const currentFile = fs.readFileSync(configPath, 'utf-8');
+  try {
+    config = JSON.parse(currentFile)
+  } catch (err) {
+    console.error(`${configPath} is invalid JSON format!`);
+    throw err;
+  }
+}
+
+if (config['workbench.colorCustomizations'] === undefined) {
+  config['workbench.colorCustomizations'] = {};
+}
+config['workbench.colorCustomizations']['statusBar.background'] = randomRGB();
+
+fs.writeFileSync(configPath, JSON.stringify(config, undefined, 4));
